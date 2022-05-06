dc217d1d023ef1fedeb243bfe87848344f1953cc
往事随风 <499393751@qq.com>
2022-05-06 21:07:41 +0800

    修改配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,43 +1,61 @@
-const { getTsSidebar } = require('./sidebar')
 // vitepress/config.js
 module.exports = {
-    title: "Mr. Zhang's blog",// 网站标题
-    description: '我的vitepress博客.', //网站描述
-    base: '/', //  部署时的路径 默认 /  可以使用二级地址 /base/
-    // lang: 'en-US', //语言
-    // 网页头部配置，引入需要图标，css，js
-    head: [
-      // 改变title的图标
-      [
-        'link',
+  title: "Mr. Zhang's blog", // 网站标题
+  description: "总结归纳学习中的知识", //网站描述
+  base: "/myBlog/", //  部署时的路径 默认 /  可以使用二级地址 /base/
+  // lang: 'en-US', //语言
+  // 网页头部配置，引入需要图标，css，js
+  head: [
+    // 改变title的图标
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/admin.png", //图片放在public文件夹下
+      },
+    ],
+  ],
+  // 主题配置
+  themeConfig: {
+    // repo: 'Bluestar123/day-blog/tree/master',
+    repo: "vuejs/vitepress", // 你的 github 仓库地址，网页的右上角会跳转
+    //   头部导航
+    nav: [
+      { text: "首页", link: "/" },
+      {
+        text: "Html/Css",
+        items: [
+          { text: "Html5", link: "/html/" },
+          { text: "Css3", link: "/css/" },
+        ],
+      },
+      // { text: 'Css', link: '/css/' },
+      { text: "JavaScript", link: "/javascript/" },
+      { text: "Vue", link: "/vue/" },
+      { text: "React", link: "/react/" },
+      { text: "微信小程序", link: "/wxwatch/" },
+      { text: "部署", link: "/deployment/" },
+    ],
+    //   侧边导航
+    sidebar: {
+      html: [
         {
-          rel: 'icon',
-          href: '/img/linktolink.png',//图片放在public文件夹下
+          text: "Html5",
+          children: [
+            { text: "概况", link: "/html/" },
+            { text: "入门", link: "/html/htmlone" },
+          ],
         },
       ],
-    ],
-    // 主题配置
-    themeConfig: {
-      repo: 'vuejs/vitepress', // 你的 github 仓库地址，网页的右上角会跳转
-      //   头部导航
-      nav: [
-        { text: '首页', link: '/' },
-        { text: 'Html', link: '/html/'},
-        { text: 'Css', link: '/css/' },
-        { text: 'JavaScript', link: '/javascript/',},
-        { text: 'Vue', link: '/vue/'},
-        { text: 'React', link: '/react/' },
-        {text:'微信小程序',link: '/wxwatch/'},
-        {text:'部署',link: '/deployment/'},
+      css: [
+        {
+          text: "Css3",
+          children: [
+            { text: "css文档", link: "/css/" },
+            { text: "css文档1", link: "/css/cssone" },
+          ],
+        },
       ],
-      //   侧边导航
-      // sidebar: [
-      //   { text: '首页', link: '/' },
-      // ]
-      // sidebar:{
-      //   '/vue/': getTsSidebar()
-      // }
     },
-      
-  }
-  
+  },
+};
